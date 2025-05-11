1fa0dc13b9ac67c8ed030030f5590647219f52ae
Sendiko <sarangtawon897@gmail.com>
2025-05-11 15:35:57 +0700

    chore: theme adjustment

--- app/src/main/java/com/github/sendiko/kostgoandroid/core/MainActivity.kt
+++ app/src/main/java/com/github/sendiko/kostgoandroid/core/MainActivity.kt
@@ -11,14 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.github.sendiko.kostgoandroid.core.ui.theme.KostandroiddummyTheme
+import com.github.sendiko.kostgoandroid.core.ui.theme.KostgoandroidTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            KostandroiddummyTheme {
+            KostgoandroidTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
@@ -41,7 +41,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    KostandroiddummyTheme {
+    KostgoandroidTheme {
         Greeting("Android")
     }
 }
